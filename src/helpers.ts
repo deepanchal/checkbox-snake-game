07baf41794091a91a8d348493908625feff1c583
Deep Panchal <deep302001@gmail.com>
2022-06-04 15:19:18 -0500

    refactor(app): add GameBoardInfo interface + refactor code

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,8 +1,31 @@
-export function setBoard(gameBoard: HTMLDivElement, rows: number, cols: number) {
-    gameBoard.style.display = 'grid';
-    gameBoard.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
-    gameBoard.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-    gameBoard.style.gap = '5px';
-    gameBoard.style.justifyContent = 'center';
-    gameBoard.style.alignItems = 'center';
+export interface GameBoardInfo {
+  boardRef: HTMLDivElement
+  rows: number
+  cols: number
+}
+
+export function setBoardStyles(options: GameBoardInfo) {
+  const { boardRef: gameBoard, rows, cols } = options
+  gameBoard.style.display = 'grid'
+  gameBoard.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
+  gameBoard.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+  gameBoard.style.gap = '1rem'
+  gameBoard.style.justifyContent = 'center'
+  gameBoard.style.alignItems = 'center'
+}
+
+export function setupBoard(options: GameBoardInfo) {
+  const { boardRef: gameBoard, rows, cols } = options
+  Array.from({ length: rows }).forEach((_, row) => {
+    Array.from({ length: cols }).forEach((_, col) => {
+      const cell = document.createElement('input')
+      cell.setAttribute('type', 'checkbox')
+      // cell.disabled = true;
+      cell.classList.add('cell')
+      cell.id = `${row}${col}`
+      cell.dataset.row = row.toString()
+      cell.dataset.col = col.toString()
+      gameBoard.appendChild(cell)
+    })
+  })
 }
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,21 @@
-import { setBoard } from './helpers';
+import { setBoardStyles, setupBoard } from './helpers'
+import type { GameBoardInfo } from './helpers'
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
-const ROWS = 10;
-const COLS = 10;
-const gameBoard = document.createElement('div');
-gameBoard.classList.add('game-board');
+const app = document.querySelector<HTMLDivElement>('#app')!
+const ROWS = 10
+const COLS = 10
+const gameBoard = document.createElement('div')
+gameBoard.classList.add('game-board')
 
-setBoard(gameBoard, ROWS, COLS);
-
-// create the board
-for (let row = 0; row < ROWS; row++) {
-  for (let col = 0; col < COLS; col++) {
-    const cell = document.createElement('input')
-    cell.setAttribute('type', 'checkbox');
-    // cell.disabled = true;
-    cell.classList.add('cell');
-    cell.id = `${row}${col}`;
-    cell.dataset.row = row.toString();
-    cell.dataset.col = col.toString();
-    gameBoard.appendChild(cell);
-  }
+const boardInfo: GameBoardInfo = {
+  boardRef: gameBoard,
+  rows: ROWS,
+  cols: COLS
 }
 
+setBoardStyles(boardInfo)
+setupBoard(boardInfo)
+
 // append game board to app
-app.appendChild(gameBoard);
+app.appendChild(gameBoard)
