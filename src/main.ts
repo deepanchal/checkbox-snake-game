5cedcfd99d2f9dadc6dde9d59cb340a2d8f884fb
Deep Panchal <deep302001@gmail.com>
2022-06-04 14:47:07 -0500

    feat: setup game board in #app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
+import { setBoard } from './helpers';
 import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
+const ROWS = 10;
+const COLS = 10;
+const gameBoard = document.createElement('div');
+gameBoard.classList.add('game-board');
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+setBoard(gameBoard, ROWS, COLS);
 
+// create the board
+for (let row = 0; row < ROWS; row++) {
+  for (let col = 0; col < COLS; col++) {
+    const cell = document.createElement('input')
+    cell.setAttribute('type', 'checkbox');
+    // cell.disabled = true;
+    cell.classList.add('cell');
+    cell.id = `${row}${col}`;
+    cell.dataset.row = row.toString();
+    cell.dataset.col = col.toString();
+    gameBoard.appendChild(cell);
+  }
+}
+
+// append game board to app
+app.appendChild(gameBoard);
